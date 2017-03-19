43e1d4459ccd4bf22b7626c3065ec8868ff913b9
zhyzhyzhy <1012146386@qq.com>
2017-03-19 14:41:43 +0800

    update

--- Learn/Algo/README.md
+++
@@ -1,8 +0,0 @@
-目录
-------
-
-* [求最大公约数](/src/gcd/gcd.java)
-
-* [二叉搜索树](/src/Search/BST.java)
-
-* [排序算法](/src/Sort/Sort.java)
--- Learn/Graph_java/src/Graph/Graph.java
+++
@@ -1,8 +0,0 @@
-package Graph;
-
-/**
- * Created by zhy on 12/2/16.
- */
-public interface Graph {
-    boolean Insert(int u, int v, int weight);
-}
