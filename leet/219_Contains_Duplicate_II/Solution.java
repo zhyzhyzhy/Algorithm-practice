8074c27b7d1a3b6bcd3140c56e8ff309d30efddb
zhyzhyzhy <1012146386@qq.com>
2017-04-06 12:26:17 +0800

    update

--- leet/219_Contains_Duplicate_II/Solution.java
+++ leet/219_Contains_Duplicate_II/Solution.java
@@ -1,11 +1,18 @@
+import java.util.Arrays;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+
 public class Solution {
-	public static boolean containsNearbyDuplicate(int[] nums, int k) {
-		if (nums == null || k < 1) {
-			return false;
-		}
-		TreeSet
-    }
-    public static void main(String[] args) {
-    	
-    }
+    public static boolean containsNeabDuplicate(int[]nums,int k){
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+}
+public static void main(String[]args){
+
+}
 }
